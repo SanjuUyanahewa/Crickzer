1ebd3f73509e77f9292c8a512eef8a1491e55067
Kalsara <kalsaramagamage@gmail.com>
2017-07-30 15:48:28 +0530

    api handling

--- app/src/main/java/com/crickzer/app/crickzer/MainScreen.java
+++ app/src/main/java/com/crickzer/app/crickzer/MainScreen.java
@@ -2,17 +2,35 @@ package com.crickzer.app.crickzer;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import java.util.ArrayList;
 
+import static com.crickzer.app.crickzer.R.id.listView1;
+
 public class MainScreen extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +38,28 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        String[] myItems1={"Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand"};
+        final String[] myItems1={"Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand"};
         String[] myItems2={"Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand","Sri Lanka Vs India","Sri Lanka Vs New Zealand","Pakistan Vs New Zealand","Sri Lanka Vs South Africa","Australia Vs New Zealand"};
         ArrayAdapter<String> adapter1=new ArrayAdapter<String>(this,R.layout. list_layout,myItems1);
-        ArrayAdapter<String> adapter2=new ArrayAdapter<String>(this,R.layout. list_layout,myItems2);
-        ListView listView1=(ListView) findViewById(R.id.listView1);
-        ListView listView2=(ListView) findViewById(R.id.listView2);
+        ListView  listView1 = (ListView) findViewById(R.id.listView1);
         listView1.setAdapter(adapter1);
-        listView2.setAdapter(adapter2);
+        ArrayAdapter<String> adapter2=new ArrayAdapter<String>(this,R.layout. list_layout,myItems2);
+       ListView listView2=(ListView) findViewById(R.id.listView2);
+       listView2.setAdapter(adapter2);
         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent,View view,int position,long id){
-                if (position==0){
-                    Intent myintent = new Intent(view.getContext(),ThirdScreen.class);
-                    startActivityForResult(myintent,0);}
-            }
+                for(int x=0;x<myItems1.length;x++){
+                    if (position == x){
+
+                        Intent myintent = new Intent(view.getContext(),ThirdScreen.class);
+                        startActivityForResult(myintent,x);}
+                    }
+                }
+
         });
     }
+
+
+
 }
--- app/src/main/java/com/crickzer/app/crickzer/ThirdScreen.java
+++ app/src/main/java/com/crickzer/app/crickzer/ThirdScreen.java
@@ -1,16 +1,185 @@
 package com.crickzer.app.crickzer;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ThirdScreen extends AppCompatActivity {
 
+    private TextView txtResponseList;
+     int ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_third_screen);
+        Button btnHitData=(Button)findViewById(R.id.hitData);
+        Button btnFirstTeam=(Button)findViewById(R.id.btnFirstTeam);
+
+        txtResponseList = (TextView)findViewById(R.id.responseList);
+        //live matches button click event
+        btnHitData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //url of the api-Matches
+
+                new JasonMatches().execute("http://cricapi.com/api/matches/XlPvSB9ReQgIquCbxhaPUDs2NLL2");
+
+                }
+        });
+        //live score button click event
+        btnFirstTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //url of the api-scores
+                new JasonScores().execute("http://cricapi.com/api/cricket/?apikey=XlPvSB9ReQgIquCbxhaPUDs2NLL2");
+
+            }
+        });
+
+    }
+
+//matches jason file
+    public class JasonMatches extends AsyncTask<String,String,String>{
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url=new URL(params[0]);
+                connection=(HttpURLConnection)url.openConnection();
+                connection.connect();
+
+                InputStream stream=connection.getInputStream();
+                reader=new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer=new StringBuffer();
+                String line="";
+
+                while ((line=reader.readLine())!=null){
+                    buffer.append(line);
+                }
+                String cricJason=buffer.toString();
+                JSONObject parentJason=new JSONObject(cricJason);
+                JSONArray arrayJasonCrick=parentJason.getJSONArray("matches");
+                StringBuffer jasonBuffer=new StringBuffer();
+
+                for(int i=0;i<arrayJasonCrick.length();i++){
+
+                    JSONObject getJasonData=arrayJasonCrick.getJSONObject(i);
+                    int teamID=getJasonData.getInt("unique_id");
+                    String firstTeam=getJasonData.getString("team-1");
+                    String secondTeam=getJasonData.getString("team-2");
+                    jasonBuffer.append("id :"+teamID+"\n"+"Team 1 :"+firstTeam+"\n"+"Team 2 :"+secondTeam+"\n\n");
+                }
+
+                return jasonBuffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null)
+                    connection.disconnect();
+                try {
+                    if(reader !=null)
+                        reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            txtResponseList.setText(result);
+
+        }
+    }
+//live score jason file
+    public class JasonScores extends AsyncTask<String,String,String>{
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url=new URL(params[0]);
+                connection=(HttpURLConnection)url.openConnection();
+                connection.connect();
+
+                InputStream stream=connection.getInputStream();
+                reader=new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer=new StringBuffer();
+                String line="";
+
+                while ((line=reader.readLine())!=null){
+                    buffer.append(line);
+                }
+                String cricJason=buffer.toString();
+                JSONObject parentJason=new JSONObject(cricJason);
+                JSONArray arrayJasonCrick=parentJason.getJSONArray("data");
+                StringBuffer jasonBuffer=new StringBuffer();
+
+                for(int i=0;i<arrayJasonCrick.length();i++){
+
+                    JSONObject getJasonData=arrayJasonCrick.getJSONObject(i);
+                   // int teamID=getJasonData.getInt("unique_id");
+                    String description=getJasonData.getString("description");
+                    jasonBuffer.append("Description"+description+"\n\n");
+                }
+
+                return jasonBuffer.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null)
+                    connection.disconnect();
+                try {
+                    if(reader !=null)
+                        reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //result set to textView
+            txtResponseList.setText(result);
+
+        }
     }
 }
